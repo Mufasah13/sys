26f3c1784f130d16e47a4e311fc3731084aebe1e
NathanNyawaya <nyawayanathan@gmail.com>
2023-04-20 22:01:06 +0300

    last streak

--- routes/assign.js
+++ routes/assign.js
@@ -1,13 +1,17 @@
 import express from "express";
 import Assign from "../models/Assigned.js";
+import User from "../models/Users.js";
 
 const router = express.Router();
 //create a statement
 
 router.post("/assigntask", async (req, res) => {
   const newAssign = new Assign(req.body);
+  const officer = await User.findOne({regNo: req.body.policeNumber})
+  await officer.addCase(req.body.caseId)
   try {
     const savedTask = await newAssign.save();
+
     //   res.status(200).json(savedTask);
     res.redirect("/a-tables");
   } catch (err) {
